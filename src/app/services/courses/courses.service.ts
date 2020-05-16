643b42388ba581d7b13b9b3974b8db2c4c2defd1
Ernesto Antonio Rodriguez Acosta <eara@localhost.localdomain>
2020-05-16 17:51:47 +0200

    Angular Custom Service - Fetching Data

--- src/app/services/courses/courses.service.ts
+++ src/app/services/courses/courses.service.ts
@@ -1,11 +1,22 @@
+import { Course } from './../../model/course';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { 
-    console.log('creating CoursesService');
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  loadCourses(): Observable<Course[]> {
+    const params = new HttpParams()
+    .set('page', '1')
+    .set('pageSize', '10');
+
+    return this.http.get<Course[]>('/api/courses', { params });
   }
 }
